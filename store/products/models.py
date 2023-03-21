aeb1d67ab620b9a66b046c8de08d1d5605da6c63
Dan7258 <italyevdm03@gmail.com>
2023-03-21 20:04:48 +0300

    21.03.2023
    авторизация пользователя

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True) # максимальное количество символов
+    description = models.TextField(null = True, blank=True) # заполнять необязательно
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(null = True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=4) # кол-во цифр до запятой и после
+    quantity = models.PositiveIntegerField(default = 0) #кол-во товаров на складе по умолчанию 0
+    image = models.ImageField(upload_to= 'products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete = models.PROTECT)# категория товара. Связали с другим классом. Что делать при удалении
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
--- store/store/settings.py
+++ store/store/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+
+    'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -121,7 +123,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Users
+
+AUTH_USER_MODEL = 'users.User'
